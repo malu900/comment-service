8e3ddc5000535ba5a37b4e7e7a21958cc20452c2
malu <malu.vandenberg@student.fontys.nl>
2022-05-18 01:07:01 +0200

    still working

--- src/main/java/com/example/commentservice/CommentServiceApplication.java
+++ src/main/java/com/example/commentservice/CommentServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.commentservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -11,5 +13,8 @@ public class CommentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentServiceApplication.class, args);
     }
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
 
 }
--- src/main/java/com/example/commentservice/config/WelcomeConfiguration.java
+++ src/main/java/com/example/commentservice/config/WelcomeConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-//@EnableConfigurationProperties
 @Configuration
 @ConfigurationProperties(prefix = "welcome")
 @Data
