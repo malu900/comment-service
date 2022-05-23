b152433609ef9c1f446a44dfda2e47de69d2ae5c
malu <malu.vandenberg@student.fontys.nl>
2022-05-23 08:27:00 +0200

    test

--- src/test/java/com/example/commentservice/CommentServiceApplicationTests.java
+++ src/test/java/com/example/commentservice/CommentServiceApplicationTests.java
@@ -1,17 +1,24 @@
-package com.example.commentservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
-class CommentServiceApplicationTests {
-
+//package com.example.commentservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//import org.springframework.util.Assert;
+//
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//@SpringBootTest
+//@Configuration
+//@EnableJpaRepositories(basePackages = "com.example.commentservice.model")
+//@PropertySource("application.properties")
+//class CommentServiceApplicationTests {
+//
 //    @Test
 //    void test() {
 //        assertTrue(true);
 //    }
-
-}
+//
+//}
